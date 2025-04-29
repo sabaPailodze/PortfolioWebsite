9688a2b0c19f3baeeff56016e48615c049a26807
saba pailodze <147437662+sabaPailodze@users.noreply.github.com>
2025-04-29 18:38:43 +0400

    refactored vite.confing.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,3 @@
-// import { defineConfig } from "vite";
-// import tailwindcss from "@tailwindcss/vite";
-// import react from "@vitejs/plugin-react";
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// });
-
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -12,13 +5,27 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    chunkSizeWarningLimit: 800,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react";
-            if (id.includes("axios")) return "axios";
-            if (id.includes("lodash")) return "lodash";
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-responsive")
+            ) {
+              return "react-vendor";
+            }
+            if (id.includes("axios")) {
+              return "axios";
+            }
+            if (id.includes("lodash")) {
+              return "lodash";
+            }
+            if (id.includes("three") || id.includes("@react-three")) {
+              return "three-vendor";
+            }
             return "vendor";
           }
         },
